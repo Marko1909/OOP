4b58c6807c5f86a3d335c639ec2c4946c81e3a09
Marko <mantolic@tvz.hr>
2023-03-22 23:28:42 +0100

    Vjezba3

--- Vjezba3.py
+++ Vjezba3.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 kolegiji = []
 
 broj_kolegija = int(input("Unesite broj kolegija: "))
@@ -6,9 +8,24 @@ for index, i in enumerate(range(broj_kolegija), start=1):
     kolegiji.append(dict(Kolegij=input("Unesite naziv kolegija: ")))
     kolegiji[i]["ECTS"] = input(f'Unesite ECTS bodove za {index}. kolegij: ')
 
-print("Popis svih kolegija: ")
-print(f'\t Kolegij {kolegiji[0]["Kolegij"]} nosi {kolegiji[0]["ECTS"]} ECTS bodova ')
-print(f'\t Kolegij {kolegiji[1]["Kolegij"]} nosi {kolegiji[1]["ECTS"]} ECTS bodova ')
-print(f'\t Kolegij {kolegiji[2]["Kolegij"]} nosi {kolegiji[2]["ECTS"]} ECTS bodova ')
 
+ispiti = []
+
+broj_ispita = int(input("Unesite broj ispita: "))
+
+for i in range(broj_ispita):
+    for index, j in enumerate(range(broj_kolegija), start=1):
+        print(f'{index}. {kolegiji[j]["Kolegij"]}')
+
+    ispiti.append(dict(Kolegij=kolegiji[int(input("Unesite kolegij: "))-1]["Kolegij"]))
+    dan = int(input("Unesite dan ispita: "))
+    mjesec = int(input("Unesite mjesec ispita: "))
+    godina = int(input("Unesite godinu ispita: "))
+    ispiti[i]["Datum"] = date(godina, mjesec, dan)
+
+
+print("Popis svih ispita: ")
+for i in range(broj_ispita):
+    print(f'\t Ispit iz kolegija {ispiti[i]["Kolegij"]}, koji nosi {kolegiji[i]["ECTS"]} ECTS bodova održati\
+će se {ispiti[i]["Datum"]}')
 
