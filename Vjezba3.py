f7e996b4e7dfb78ce0e4a59fb7d822ef3aa80969
Marko <mantolic@tvz.hr>
2023-03-22 21:24:15 +0100

    vjezbe

---
+++ Vjezba3.py
@@ -0,0 +1,15 @@
+kolegiji = []
+
+Kolegij = {}
+
+broj_kolegija = int(input("Upište broj kolegija: "))
+
+for index, i in enumerate(range(broj_kolegija), start=1):
+    Kolegij[""] = input("Unesite naziv kolegija: ")
+
+
+    #kolegiji.append(input("Unesite naziv kolegija: "))
+
+    #ects.append(input(f'Unesite ECTS bodove za {index}. kolegij: '))
+
+
