a1e8e8a5ab35bdf54b05f3a431223990e1065cca
Marko <mantolic@tvz.hr>
2023-03-22 22:34:39 +0100

    Vj3

--- Vjezba3.py
+++ Vjezba3.py
@@ -1,15 +1,14 @@
 kolegiji = []
 
-Kolegij = {}
-
-broj_kolegija = int(input("Upište broj kolegija: "))
+broj_kolegija = int(input("Unesite broj kolegija: "))
 
 for index, i in enumerate(range(broj_kolegija), start=1):
-    Kolegij[""] = input("Unesite naziv kolegija: ")
+    kolegiji.append(dict(Kolegij=input("Unesite naziv kolegija: ")))
+    kolegiji[i]["ECTS"] = input(f'Unesite ECTS bodove za {index}. kolegij: ')
 
-
-    #kolegiji.append(input("Unesite naziv kolegija: "))
-
-    #ects.append(input(f'Unesite ECTS bodove za {index}. kolegij: '))
+print("Popis svih kolegija: ")
+print(f'\t Kolegij {kolegiji[0]["Kolegij"]} nosi {kolegiji[0]["ECTS"]} ECTS bodova ')
+print(f'\t Kolegij {kolegiji[1]["Kolegij"]} nosi {kolegiji[1]["ECTS"]} ECTS bodova ')
+print(f'\t Kolegij {kolegiji[2]["Kolegij"]} nosi {kolegiji[2]["ECTS"]} ECTS bodova ')
 
 
