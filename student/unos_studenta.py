9c3b6cab5e35a09bfe60d1ad34743cbcfd585fd9
Marko <mantolic@tvz.hr>
2023-05-09 16:31:41 +0200

    Vjezba7

--- student/unos_studenta.py
+++ student/unos_studenta.py
@@ -1,8 +1,16 @@
 from ispiti import get_ispit
 from utilities import unos_intervala
-from .student import Student
+from redovni_student import RedovniStudent
+from vanredni_student import VanredniStudent
+
 
 def unos_studenta(ispiti, redni_broj):
+
+    print('Odaberite tip studenta: ')
+    print('\t1. Redovni student ')
+    print('\t2. Vanredni student ')
+    tip_studenta = int(input('unesite tip studenta: '))
+
     ime = input(f"Unesite ime {redni_broj}. studenta: ").capitalize()
     prezime = input(f"Unesite prezime {redni_broj}. studenta: ").capitalize()
 
@@ -13,4 +21,10 @@ def unos_studenta(ispiti, redni_broj):
     odabrani_ispit = unos_intervala(1,i)
     ispit = ispiti[odabrani_ispit - 1]
 
-    return Student(ime, prezime, ispit)
+    if tip_studenta == 1:
+        broj_prijava = input('Unesite broj prijava: ')
+
+        return RedovniStudent(ime, prezime, ispit, broj_prijava)
+
+    elif tip_studenta == 2:
+        return VanredniStudent(ime, prezime,ispit)
